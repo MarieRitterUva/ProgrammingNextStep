f6256a917995ff92dfb8fdad344d5b85777b0020
MarieRitterUva <marie.ritter@student.uva.nl>
2016-05-17 17:57:25 +0200

    Simulation implemented in several functions for easy access.

--- Program.R
+++ Program.R
@@ -2,16 +2,23 @@
 ## Marie Ritter - UvA
 
 # ToDo: GRAPHS!
+# ToDo: Functions out of thing
+# ToDo: Set default for init functions?
 # ToDO: Calculation of parameters?!
 # ToDo: Multiple simulations
 # ToDo: Success output
 # ToDo: Offer possibility of treatment weeks (if-statements, etc.)
-# ToDo: Write all in functions -> easy access
-# ToDO: Simple GUI input and outpur
+# ToDo: Write input ifs
+# ToDO: Simple GUI input and output
 # ToDo: Display Logic GUI
 # ToDo: Speed of Simulation
 # ToDo: Beautify GUI
 # ToDo: Manual and Instructions
+
+#############################################################################
+
+rm(list = ls())
+setwd("/home/marie/Dokumente/Uni/Courses/Programming/Repo/ProgrammingNextStep")
 
 #############################################################################
 
@@ -29,90 +36,124 @@ C.init <- 0  # craving at time 0
 
 A.init <- 0.5 * q  # consumption at time 0
 
-weeks <- 6  # number of weeks
+weeks <- 20  # number of weeks
 
 lamda.init <- 0.5  # intensity of external influences
 
 #############################################################################
-# basic simulation
-
+# Functions
 
 # calculate parameters
+# - needs to be done in beginning and if input parameters change
 
-d <- p/2
-
-k <- (p * S.plus) / q
-
-h <- p * S.plus
-
-b <- (2*d) / q
+CalculateParameters <- function (p, S.plus, q) {
+        d <<- p/2
+        k <<- (p * S.plus) / q
+        h <<- p * S.plus
+        b <<- (2*d) / q
+}
 
 
 # initialize vectors
+# - needs to be done in beginning and if input parameters change
 
-C <- numeric(length = weeks+1)
-C[1] <- C.init
-
-S <- numeric(length = weeks+1)
-S[1] <- S.plus
-
-E <-  numeric(length = weeks+1)
-E[1] <- E.init
-for (i in 1:weeks) {
-        E[i+1] <- round((E[i] - d * E[1]), 2)
-}
-
-lamda <- numeric(length = weeks+1)
-lamda[1] <- lamda.init
-for (i in 1:weeks) {
-        lamda[i+1] <- lamda[i] + d * lamda[1]
-}
-
-cues <- numeric(length = weeks+1)
-
-V <- numeric(length = weeks+1)
-V[1] <- min(c(1, max(c(0, (S.plus - C[1] - E[1]) )) ))
-
-A <- numeric(length = weeks+1)
-A[1] <- A.init
-
-
-
-
-# calculate C, S, V, and A
-
-for (i in 1:weeks) {
-        # calculate C(t+1)
-        C[i+1] <- C[i] + b * min(c(1, 1-C[i])) * A[i] - d * C[i]
+InitializeVectors <- 
+        function (C.init, S.plus, E.init, lamda.init, A.init, weeks) {
         
-        # calculate S(t+1)
-        S[i+1] <- S[i] + p * max(c(0, (S.plus - S[i])) ) - h * C[i] - k * A[i]
+        Crav <<- numeric(length = weeks+1)
+        Crav[1] <- C.init
         
-        # calculate V(t+1)
-        V[i+1] <- min(c(1, max(c(0, C[i] - S[i] - E[i]))))
+        S <<- numeric(length = weeks+1)
+        S[1] <<- S.plus
         
-        # calculate A(t+1) with random component
-        # cues
-        cues[i] <- rpois(1, lamda[i])
-        f <- cues[i] * (q/7)
-        
-        if (f <= (q * (1 - V[i])) ) {
-                
-        # calculate A(t+1)
-        A[i+1] <- q * V[i] + f
-        
-        } else {
-                A[i+1] <- q
+        E <<-  numeric(length = weeks+1)
+        E[1] <<- E.init
+        for (i in 1:weeks) {
+                E[i+1] <<- round((E[i] - d * E[1]), 2)
         }
+        
+        lamda <<- numeric(length = weeks+1)
+        lamda[1] <<- lamda.init
+        for (i in 1:weeks) {
+                lamda[i+1] <<- lamda[i] + d * lamda[1]
+        }
+        
+        cues <<- numeric(length = weeks+1)
+        
+        V <<- numeric(length = weeks+1)
+        V[1] <<- min(c(1, max(c(0, (S.plus - Crav[1] - E[1]) )) ))
+        
+        A <<- numeric(length = weeks+1)
+        A[1] <<- A.init
 }
+
+
+# Simulate C, S, V, and A
+
+SimulateAddictionComponents <- function (Crav, S, V, A, E, lamda, cues, weeks, b, d, p, S.plus, h, k, q) {
+        
+        for (i in 1:weeks) {
+                # calculate C(t+1)
+                Crav[i+1] <- Crav[i] + b * min(c(1, 1-Crav[i])) * A[i] - d * Crav[i]
+                
+                # calculate S(t+1)
+                S[i+1] <- S[i] + p * max(c(0, (S.plus - S[i])) ) - h * Crav[i] - k * A[i]
+                
+                # calculate V(t+1)
+                V[i+1] <- min(c(1, max(c(0, Crav[i] - S[i] - E[i]))))
+                
+                # calculate A(t+1) with random component
+                set.seed(1)  # for testing purposes - TAKE OUT AT THE END!!!
+                cues[i] <- rpois(1, lamda[i])
+                f <- cues[i] * (q/7)
+                
+                if (f <= (q * (1 - V[i])) ) {  # stay under max
+                        
+                        A[i+1] <- q * V[i] + f
+                        
+                } else {
+                        A[i+1] <- q
+                }
+        }
+        
+        cues <<- cues
+        A <<- A
+        Crav <<- Crav
+        S <<- S
+        V <<- V
+        
+}
+
+
+# build output dataframe
+
+BuildOutputDataframe <- function (weeks, A, Crav, S, E, lamda, cues, V) {
+        output <- data.frame("t" = (1: (weeks+1))-1,  "A" = A, "C" = Crav, "S" = S,
+                             "E" = E, "lamda" = lamda, "cues" = cues, "V" = V)
+        return(output)
+}
+
+###############################################################################
+# initializing
+
+CalculateParameters(p, S.plus, q)
+
+InitializeVectors(C.init, S.plus, E.init, lamda.init, A.init, weeks)
+
+
+# actual simulation
+
+SimulateAddictionComponents(Crav, S, V, A, E, lamda, cues, weeks, b, d, p, S.plus, h, k, q)
+
+
+# get output
+
+BuildOutputDataframe(weeks, A, Crav, S, E, lamda, cues, V)
 
 
 ###############################################################################
 
-# output
-data <- data.frame("t" = (1: (weeks+1))-1,  "A" = A, "C" = C, "S" = S,
-                   "E" = E, "lamda" = lamda, "cues" = cues, "V" = V)
 
-data
+
 
 
